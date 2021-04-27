459621a2a79efde914cef94813a967f8c32c8050
AdrianAbeyta <54719810+AdrianAbeyta@users.noreply.github.com>
2021-04-26 21:42:24 -0500

    Add files via upload

---
+++ src/negative_obstacle_detection.h
@@ -0,0 +1,69 @@
+#include <ros/ros.h> 
+#include <vector>
+#include <list>
+#include <sensor_msgs/PointCloud.h>
+#include "eigen3/Eigen/Dense"
+#include "../shared/math/geometry.h"
+#include "../shared/math/line2d.h"
+#include "nav_msgs/OccupancyGrid.h"
+#include "sensor_msgs/LaserScan.h"
+#include "geometry_msgs/TransformStamped.h"
+#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/PoseStamped.h"
+
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl_ros/point_cloud.h>
+#include <tf2_ros/buffer.h>
+
+
+namespace ros {
+  class NodeHandle;
+}
+
+namespace NegativeObstacle{
+
+    struct Array {
+
+      std::vector<double> range;
+      std::vector<double> angle;
+      std::vector<double> index;
+
+    };
+
+    class FilterPCL{
+      public:
+
+      pcl::PointCloud<pcl::PointXYZ>::Ptr NegativeLimitFilter( const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered );
+
+      pcl::PointCloud<pcl::PointXYZ>::Ptr FloorProjection( const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered );
+
+      pcl::PointCloud<pcl::PointXYZ>::Ptr PointsToFloor( const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered );
+
+    };
+
+    class FarthestPoint{
+      public:
+        // sensor_msgs::LaserScan FillLaserMsg( sensor_msgs::LaserScan  array , pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered);
+        sensor_msgs::LaserScan FurthestPointExtraction(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered);
+        //sensor_msgs::LaserScan VirtualFloorProjection(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered);
+        sensor_msgs::LaserScan VirtualFloorProjection(sensor_msgs::PointCloud2 lidar_cloud);
+        //sensor_msgs::LaserScan PointToLaser( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered);
+        sensor_msgs::LaserScan PointToLaser(const sensor_msgs::PointCloud2ConstPtr& cloud_filtered );
+        
+        sensor_msgs::LaserScan CombineLaserScans(sensor_msgs::LaserScan  array_a, sensor_msgs::LaserScan  array_b , sensor_msgs::LaserScan  array_c);
+        sensor_msgs::LaserScan CombineAllScans( sensor_msgs::LaserScan  array_a, sensor_msgs::LaserScan  array_b , sensor_msgs::LaserScan  array_c,sensor_msgs::LaserScan  array_d,sensor_msgs::LaserScan  array_e);
+        // sensor_msgs::LaserScan RampDetection( sensor_msgs::LaserScan  array , pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered);
+        void NegativeProjection( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
+    };
+
+    
+    // class VirtualFloor{
+    //   public:
+         
+    //   private:
+
+    // }
+}
